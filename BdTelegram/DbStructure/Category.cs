e31de73a6442e4a8517caca1c888cc483ee39cf9
Андрей Захаров <diagenin@gmail.com>
2022-02-09 18:20:02 +0300

    Создание основных классов для будущей БД.

---
+++ BdTelegram/DbStructure/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdTelegram.DbStructure
+{
+    class Category
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ BdTelegram/DbStructure/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdTelegram.DbStructure
+{
+    class Product
+    {
+        int IdProduct { get; set; }
+        string Name { get; set; }
+
+        int Count { get; set; }
+        string Description { get; set; }
+        string LinkDownloading { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
